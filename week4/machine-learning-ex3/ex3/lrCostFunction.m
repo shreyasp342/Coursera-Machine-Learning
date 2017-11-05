32d72b8659734113bc4ba1d1f96a3860d602055e
Shreyas Prabhakar <shreyas.p342@gmail.com>
2017-11-05 13:52:35 -0500

    week 4 PART-1 multi-class classification submitted

--- week4/machine-learning-ex3/ex3/lrCostFunction.m
+++ week4/machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X*theta);
+J = ((1/m)*sum(-y.*log(h) - (1-y).*log(1-h))) + ((lambda/(2*m)) * sum(theta(2:end).^2));
 
-
-
-
-
-
-
-
+grad = (1/m) .* (X' * (h-y));
+grad(2:end) = grad(2:end) + ((lambda/m)*theta(2:end));
 
 % =============================================================
 
